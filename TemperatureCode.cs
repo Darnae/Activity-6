b4e14b53628c78eb6bd8f50db2031a39b9f1bd13
Darnae <108689727+Darnae@users.noreply.github.com>
2022-08-01 20:15:53 -0400

    Add files via upload

---
+++ TemperatureCode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double fahrenheight = 80;
+            double Celcius = (fahrenheight - 32) * 5 / 9;
+
+            Celciustextbox.Text = Celcius.ToString("N3");
+
+            
+        }
+
+        private void Celcius_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
